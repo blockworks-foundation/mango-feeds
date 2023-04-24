199c2c6015504e6d7e3276599f50b8a337fc5ce0
Riordan Panayides <riordan@panayid.es>
2023-04-24 19:19:18 +0100

    Refactor client library, add orderbook feed client

---
+++ ts/client/src/orderbook.ts
@@ -0,0 +1,100 @@
+import { ReconnectingWebsocketFeed } from './util';
+
+interface OrderbookFeedOptions {
+  subscriptions?: OrderbookFeedSubscribeParams;
+  reconnectionIntervalMs?: number;
+  reconnectionMaxAttempts?: number;
+}
+
+interface OrderbookFeedSubscribeParams {
+  marketId?: string;
+  marketIds?: string[];
+}
+
+interface OrderbookL2Update {
+  market: string,
+  side: 'bid' | 'ask',
+  update: [number, number][],
+  slot: number,
+  writeVersion: number,
+}
+
+function isOrderbookL2Update(obj: any): obj is OrderbookL2Update {
+  return obj.update !== undefined;
+}
+
+interface OrderbookL2Checkpoint {
+  market: string,
+  side: 'bid' | 'ask',
+  bids: [number, number][],
+  asks: [number, number][],
+  slot: number,
+  writeVersion: number,
+}
+
+function isOrderbookL2Checkpoint(obj: any): obj is OrderbookL2Checkpoint {
+  return obj.bids !== undefined && obj.asks !== undefined;
+}
+
+export class OrderbookFeed extends ReconnectingWebsocketFeed {
+  private _subscriptions?: OrderbookFeedSubscribeParams;
+
+  private _onL2Update: ((update: OrderbookL2Update) => void) | null = null;
+  private _onL2Checkpoint: ((update: OrderbookL2Checkpoint) => void) | null =
+    null;
+
+  constructor(url: string, options?: OrderbookFeedOptions) {
+    super(
+      url,
+      options?.reconnectionIntervalMs,
+      options?.reconnectionMaxAttempts,
+    );
+    this._subscriptions = options?.subscriptions;
+
+    this.onMessage((data) => {
+      if (isOrderbookL2Update(data) && this._onL2Update) {
+        this._onL2Update(data);
+      } else if (isOrderbookL2Checkpoint(data) && this._onL2Checkpoint) {
+        this._onL2Checkpoint(data);
+      }
+    });
+
+    if (this._subscriptions !== undefined) {
+      this.subscribe(this._subscriptions);
+    }
+  }
+
+  public subscribe(subscriptions: OrderbookFeedSubscribeParams) {
+    if (this.connected()) {
+      this._socket.send(
+        JSON.stringify({
+          command: 'subscribe',
+          ...subscriptions,
+        }),
+      );
+    } else {
+      console.warn('[OrderbookFeed] attempt to subscribe when not connected');
+    }
+  }
+
+  public unsubscribe(marketId: string) {
+    if (this.connected()) {
+      this._socket.send(
+        JSON.stringify({
+          command: 'unsubscribe',
+          marketId,
+        }),
+      );
+    } else {
+      console.warn('[OrderbookFeed] attempt to unsubscribe when not connected');
+    }
+  }
+
+  public onL2Update(callback: (update: OrderbookL2Update) => void) {
+    this._onL2Update = callback;
+  }
+
+  public onL2Checkpoint(callback: (checkpoint: OrderbookL2Checkpoint) => void) {
+    this._onL2Checkpoint = callback;
+  }
+}
---
+++ ts/client/src/util.ts
@@ -0,0 +1,118 @@
+import ws from 'ws';
+
+const WebSocket = global.WebSocket || ws;
+
+interface StatusMessage {
+  success: boolean;
+  message: string;
+}
+
+function isStatusMessage(obj: any): obj is StatusMessage {
+  return obj.success !== undefined;
+}
+
+export class ReconnectingWebsocketFeed {
+  private _url: string;
+  protected _socket: WebSocket;
+  private _connected: boolean;
+  private _reconnectionIntervalMs: number;
+  private _reconnectionMaxAttempts: number;
+  private _reconnectionAttempts: number;
+
+  private _onConnect: (() => void) | null = null;
+  private _onDisconnect:
+    | ((reconnectionAttemptsExhausted: boolean) => void)
+    | null = null;
+  private _onStatus: ((update: StatusMessage) => void) | null = null;
+  private _onMessage: ((data: any) => void) | null = null;
+
+  constructor(
+    url: string,
+    reconnectionIntervalMs?: number,
+    reconnectionMaxAttempts?: number,
+  ) {
+    this._url = url;
+    this._reconnectionIntervalMs = reconnectionIntervalMs ?? 5000;
+    this._reconnectionMaxAttempts = reconnectionMaxAttempts ?? -1;
+    this._reconnectionAttempts = 0;
+
+    this._connect();
+  }
+
+  public disconnect() {
+    if (this._connected) {
+      this._socket.close();
+      this._connected = false;
+    }
+  }
+
+  public connected(): boolean {
+    return this._connected;
+  }
+
+  public onConnect(callback: () => void) {
+    this._onConnect = callback;
+  }
+
+  public onDisconnect(callback: (reconnectionAttemptsExhausted: boolean) => void) {
+    this._onDisconnect = callback;
+  }
+
+  public onStatus(callback: (update: StatusMessage) => void) {
+    this._onStatus = callback;
+  }
+
+  protected onMessage(callback: (data: any) => void) {
+    this._onMessage = callback;
+  }
+
+  private _connect() {
+    this._socket = new WebSocket(this._url);
+
+    this._socket.addEventListener('error', (err: any) => {
+      console.warn(`[MangoFeed] connection error: ${err.message}`);
+      if (this._reconnectionAttemptsExhausted()) {
+        console.error('[MangoFeed] fatal connection error');
+        throw err.error;
+      }
+    });
+
+    this._socket.addEventListener('open', () => {
+      this._connected = true;
+      this._reconnectionAttempts = 0;
+      if (this._onConnect) this._onConnect();
+    });
+
+    this._socket.addEventListener('close', () => {
+      this._connected = false;
+      setTimeout(() => {
+        if (!this._reconnectionAttemptsExhausted()) {
+          this._reconnectionAttempts++;
+          this._connect();
+        }
+      }, this._reconnectionIntervalMs);
+      if (this._onDisconnect)
+        this._onDisconnect(this._reconnectionAttemptsExhausted());
+    });
+
+    this._socket.addEventListener('message', (msg: any) => {
+      try {
+        const data = JSON.parse(msg.data);
+        if (isStatusMessage(data) && this._onStatus) {
+          this._onStatus(data);
+        } else if (this._onMessage) {
+          this._onMessage(data);
+        }
+      } catch (err) {
+        console.warn('[MangoFeed] error deserializing message', err);
+      }
+    });
+  }
+
+  private _reconnectionAttemptsExhausted(): boolean {
+    return (
+      this._reconnectionMaxAttempts != -1 &&
+      this._reconnectionAttempts >= this._reconnectionMaxAttempts
+    );
+  }
+}
