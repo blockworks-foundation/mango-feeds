9723ea9f6575788103b8885dd100fbb26f4a0f53
Lou-Kamades <lou_kamades@proton.me>
2023-08-30 13:10:31 +0200

    temp

---
+++ service-mango-swaps/src/main.rs
@@ -0,0 +1,21 @@
+use mango_v4::logs::{FlashLoanLog, FlashLoanTokenDetail};
+
+fn main() {
+
+    println!("Hello, world!");
+
+    // use geyser to listen to txns
+
+    // TODO: where's the v2?? log events
+
+    // let x = FlashLoanTokenDetail {
+    //     token_index: position.token_index,
+    //     change_amount: change.amount.to_bits(),
+    //     loan: loan.to_bits(),
+    //     loan_origination_fee: loan_origination_fee.to_bits(),
+    //     deposit_index: bank.deposit_index.to_bits(),
+    //     borrow_index: bank.borrow_index.to_bits(),
+    //     price: oracle_price.to_bits(),
+    //     swap_fee: swap_fee.to_bits(),
+    // };
+}
