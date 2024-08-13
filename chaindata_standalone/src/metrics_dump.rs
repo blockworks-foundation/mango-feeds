90b7061fb3b47c021c30a3b617fed25dbdb6131c
GroovieGermanikus <groovie@mango.markets>
2024-08-13 10:27:53 +0200

    prom throughput

---
+++ chaindata_standalone/src/metrics_dump.rs
@@ -0,0 +1,38 @@
+use std::f64::NAN;
+use std::time::Duration;
+use log::{info, trace};
+use prometheus::core::{Collector, Metric};
+use prometheus::IntCounter;
+use tokio::time::{Instant, Interval, interval, sleep};
+use crate::SOME_METRIC;
+
+pub fn start_metrics_dumper(prom_counter: &IntCounter) {
+    let metric = prom_counter.clone();
+    let name = prom_counter.desc().get(0).map(|x| x.fq_name.clone()).unwrap_or("noname".to_string());
+
+    tokio::spawn(async move {
+
+        let mut interval = interval(Duration::from_millis(3000));
+
+        let mut last_observed_at = Instant::now();
+        let mut last_observed_value: u64 = u64::MIN;
+
+        loop {
+            let value = metric.get();
+            trace!("counter {} value: {}", name, value);
+
+            if last_observed_value != u64::MIN {
+                let elapsed = last_observed_at.elapsed().as_secs_f64();
+                let delta = value - last_observed_value;
+                info!("counter <{}> (value={}) with throughput {:.1}/s", name, value, delta as f64 / elapsed);
+            }
+
+            last_observed_value = value;
+            last_observed_at = Instant::now();
+
+            interval.tick().await;
+        }
+
+
+    });
+}
